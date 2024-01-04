7cad373078222c77847cb30506ebb23edd3785b1
Akash Behara <behara.akash68@gmail.com>
2024-01-04 15:25:16 +0530

    stripe webhook

---
+++ pages/api/stripe-webhook.ts
@@ -0,0 +1,45 @@
+import { buffer } from "micro";
+import { NextApiRequest, NextApiResponse } from "next";
+import Stripe from "stripe"
+
+export const config = {
+    api: {
+        bodyParser: false
+    }
+}
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY! as string, {
+    apiVersion: "2023-10-16"
+});
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse){
+    const buf = await buffer(req);
+    const sig = req.headers["stripe-signature"] as string;
+
+    if(!sig) {
+        return res.status(400).json({message: "Missing Stripe signature"});
+    }
+
+    let event: Stripe.Event;
+
+    try {
+        event = stripe.webhooks.constructEvent(buf.toString(), sig, process.env.STRIPE_WEBHOOK_SECRET! as string)
+    } catch (error) {
+       return res.status(400).json({message: `Invalid Stripe signature ${error}`});
+    }
+
+    switch(event.type) {
+        case 'charge.succeeded':
+            const charge: any = event.data.object as Stripe.Charge;
+            if(typeof charge.payment_intent === "string") {
+                await prisma?.order.update({
+                    where: {paymentIntentId: charge.payment_intent},
+                    data: {status: 'complete', address: charge.shipping?.address}
+                })
+            }
+            break
+        default: console.log('Unhandled event type', event.type);
+    }
+
+    res.json({received: true});
+}
